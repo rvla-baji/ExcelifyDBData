38afa3874846665558f683d2e4e88c7e97b2f0be
Baji <bajiravella22@gmail.com>
2023-12-06 09:33:02 +0530

    Initial commit

---
+++ dataexport-api/src/main/java/io/dataexport/dataexport/DataexportApiApplication.java
@@ -0,0 +1,13 @@
+package io.dataexport.dataexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataexportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataexportApiApplication.class, args);
+	}
+
+}
