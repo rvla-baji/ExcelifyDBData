a720965a134a7e0815dc28e4ee1b919da854ff2d
rvla-baji <bajiravella22@gmail.com>
2023-12-06 18:32:13 +0530

    Entity & Repository

--- dataexport-api/src/main/java/io/dataexport/dataexport/DataexportApiApplication.java
+++ dataexport-api/src/main/java/io/dataexport/dataexport/DataexportApiApplication.java
@@ -8,7 +8,6 @@ public class DataexportApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataexportApiApplication.class, args);
-
 	}
 
 }
---
+++ dataexport-api/src/main/java/io/dataexport/dataexport/entity/Organization.java
@@ -0,0 +1,28 @@
+package io.dataexport.dataexport.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "organization")
+@Getter
+@Setter
+public class Organization {
+
+	@Id
+	@Column(name = "emp_id")
+	private Integer empId;
+
+	@Column(name = "emp_name")
+	private String empName;
+
+	@Column(name = "emp_technology")
+	private String empTechnology;
+
+	@Column(name = "emp_location")
+	private String empLocation;
+}
