ffb4e94443cee78bb2f2e7f8ac1cdee8fd1a10d9
rvla-baji <bajiravella22@gmail.com>
2023-12-07 07:16:48 +0530

    add s3nimbusuploader-api

---
+++ s3nimbusuploader-api/src/main/java/io/s3nimbussquad/S3nimbusuploaderApplication.java
@@ -0,0 +1,13 @@
+package io.s3nimbussquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3nimbusuploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3nimbusuploaderApplication.class, args);
+	}
+
+}
---
+++ s3nimbusuploader-api/src/test/java/io/s3nimbussquad/S3nimbusuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.s3nimbussquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3nimbusuploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
