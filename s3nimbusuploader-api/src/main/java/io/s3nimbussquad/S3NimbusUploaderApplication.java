975904d0ec12e046fbb9c2690b59c611262c49c9
rvla-baji <bajiravella22@gmail.com>
2024-01-10 13:06:46 +0530

    renamed main class name

--- s3nimbusuploader-api/src/main/java/io/s3nimbussquad/S3NimbusUploaderApplication.java
+++ s3nimbusuploader-api/src/main/java/io/s3nimbussquad/S3NimbusUploaderApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class S3nimbusuploaderApplication {
+public class S3NimbusUploaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(S3nimbusuploaderApplication.class, args);
+		SpringApplication.run(S3NimbusUploaderApplication.class, args);
 	}
-
 }
