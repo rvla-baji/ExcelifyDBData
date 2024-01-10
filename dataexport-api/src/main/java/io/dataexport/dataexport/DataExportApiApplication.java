4a066e1e3f3a8a0da4b6fc399146c8b8f1913a9d
rvla-baji <bajiravella22@gmail.com>
2024-01-10 13:11:42 +0530

    main class name re-named

--- dataexport-api/src/main/java/io/dataexport/dataexport/DataExportApiApplication.java
+++ dataexport-api/src/main/java/io/dataexport/dataexport/DataExportApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataexportApiApplication {
+public class DataExportApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataexportApiApplication.class, args);
+		SpringApplication.run(DataExportApiApplication.class, args);
 	}
 
 }
