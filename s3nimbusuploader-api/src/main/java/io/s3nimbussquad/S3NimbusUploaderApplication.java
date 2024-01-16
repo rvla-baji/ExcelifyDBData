a0e9acbd5873bd1953a32f50b9736b272d087fdb
rvla-baji <bajiravella22@gmail.com>
2024-01-16 21:43:50 +0530

    feign client dependency

--- s3nimbusuploader-api/src/main/java/io/s3nimbussquad/S3NimbusUploaderApplication.java
+++ s3nimbusuploader-api/src/main/java/io/s3nimbussquad/S3NimbusUploaderApplication.java
@@ -2,8 +2,10 @@ package io.s3nimbussquad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class S3NimbusUploaderApplication {
 
 	public static void main(String[] args) {
