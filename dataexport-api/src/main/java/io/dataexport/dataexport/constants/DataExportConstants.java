56bc249719a17b77bccc0c6dd5c55e8c6112abaf
rvla-baji <bajiravella22@gmail.com>
2024-01-17 22:24:11 +0530

    web services code added

--- dataexport-api/src/main/java/io/dataexport/dataexport/constants/DataExportConstants.java
+++ dataexport-api/src/main/java/io/dataexport/dataexport/constants/DataExportConstants.java
@@ -4,5 +4,5 @@ public class DataExportConstants {
 	public static final String FONT_STYLE = "TIMES NEW ROMAN";
 	public static final String SHEET_NAME = "Employees_Details";
 
-
+public static final String NIMBUS_UPLOAD_END_POINT="http://localhost:9090/nimbusUpload/s3bucket";
 }
